4b1527336e4a4baada2662ffd8263b72a76b418f
Daniel Simpson <40238765+danielbsimpson@users.noreply.github.com>
2020-07-09 17:11:31 +0100

    Added colors and deaths by state

--- Covid_bar_charts_animation.py
+++ Covid_bar_charts_animation.py
@@ -15,80 +15,116 @@ from IPython.display import HTML
 data = pd.read_csv("C:/Users/Damien/Desktop/Data Science Projects/Covid/us-counties.csv")
 data['date'] = pd.to_datetime(data['date'])
 data = data.drop(columns = ['fips', 'county'])
+data['month'] = data['date'].dt.strftime('%b')
+
+state_list = list(data['state'].unique())
+color_list = ['#7e1e9c', '#15b01a', '#0343df', '#ff81c0', '#653700', '#e50000',
+              '#95d0fc', '#029386', '#f97306', '#96f97b', '#c20078', '#ffff14',
+              '#929591', '#89fe05', '#75bbfd', '#bf77f6', '#9a0eea', '#033500',
+              '#06c2ac', '#c79fef', '#00035b', '#d1b26f', '#00ffff', '#13eac9',
+              '#06470c', '#ae7181', '#ed0dd9', '#01ff07', '#650021', '#6e750e',
+              '#ff796c', '#e6daa6', '#0504aa', '#a2cffe', '#cea2fd', '#840000',
+              '#ff028d', '#ad8150', '#c7fdb5', '#ffb07c', '#677a04', '#cb416b',
+              '#8e82fe', '#53fca1', '#aaff32', '#380282', '#ceb301', '#ffd1df',
+              '#cf6275', '#0165fc']
+state_color = dict(zip(state_list, color_list))
 
 def get_week(data, date):
-    desired_week = data[data['date'] == date].groupby('state').agg({'cases':'sum'})
-    desired_week = desired_week.nlargest(10, 'cases')
-    return desired_week
+    desired_week = data[data['date'] == date].groupby('state').agg({
+        'cases':'sum','deaths':'sum'})
+    cases_week = desired_week.nlargest(10, 'cases')
+    deaths_week = desired_week.nlargest(10, 'deaths')
+    
+    return (cases_week, deaths_week)
 
-jan_week = get_week(data, '2020-01-31')
-jan_week.loc['Massachusetts'] = [0]
-jan_week.loc['Wisconsin'] = [0]
-jan_week.loc['Texas'] = [0]
-jan_week.loc['Nebraska'] = [0]
-jan_week.loc['Utah'] = [0]
-jan_week.loc['Oregon'] = [0]
+jan_cases = get_week(data, '2020-01-31')[0]
+jan_cases.loc['Massachusetts'] = [0,0]
+jan_cases.loc['Wisconsin'] = [0,0]
+jan_cases.loc['Texas'] = [0,0]
+jan_cases.loc['Nebraska'] = [0,0]
+jan_cases.loc['Utah'] = [0,0]
+jan_cases.loc['Oregon'] = [0,0]
+jan_cases['month'] = 'January'
 
-feb_week1 = get_week(data, '2020-02-08')
-feb_week1.loc['Texas'] = [0]
-feb_week1.loc['Nebraska'] = [0]
-feb_week1.loc['Utah'] = [0]
-feb_week1.loc['Oregon'] = [0]
+feb_cases1 = get_week(data, '2020-02-08')[0]
+feb_cases1.loc['Texas'] = [0,0]
+feb_cases1.loc['Nebraska'] = [0,0]
+feb_cases1.loc['Utah'] = [0,0]
+feb_cases1.loc['Oregon'] = [0,0]
+feb_cases1['month'] = 'February'
 
-feb_week2 = get_week(data, '2020-02-15')
-feb_week2.loc['Nebraska'] = [0]
-feb_week2.loc['Utah'] = [0]
-feb_week2.loc['Oregon'] = [0]
+feb_cases2 = get_week(data, '2020-02-15')[0]
+feb_cases2.loc['Nebraska'] = [0,0]
+feb_cases2.loc['Utah'] = [0,0]
+feb_cases2.loc['Oregon'] = [0,0]
+feb_cases2['month'] = 'February'
 
-feb_week3 = get_week(data, '2020-02-22')
-feb_week3.loc['Oregon'] = [0]
-feb_week3.loc['Utah'] = [0]
+feb_cases3 = get_week(data, '2020-02-22')[0]
+feb_cases3.loc['Oregon'] = [0,0]
+feb_cases3.loc['Utah'] = [0,0]
+feb_cases3['month'] = 'February'
 
-feb_week4 = get_week(data, '2020-02-29')
+feb_cases4 = get_week(data, '2020-02-29')[0]
+feb_cases4['month'] = 'February'
 
-mar_week1 = get_week(data, '2020-03-07')
-mar_week2 = get_week(data, '2020-03-14')
-mar_week3 = get_week(data, '2020-03-21')
-mar_week4 = get_week(data, '2020-03-28')
+mar_cases1 = get_week(data, '2020-03-07')[0]
+mar_cases1['month'] = 'March'
+mar_cases2 = get_week(data, '2020-03-14')[0]
+mar_cases2['month'] = 'March'
+mar_cases3 = get_week(data, '2020-03-21')[0]
+mar_cases3['month'] = 'March'
+mar_cases4 = get_week(data, '2020-03-28')[0]
+mar_cases4['month'] = 'March'
 
-apr_week1 = get_week(data, '2020-04-04')
-apr_week2 = get_week(data, '2020-04-11')
-apr_week3 = get_week(data, '2020-04-18')
-apr_week4 = get_week(data, '2020-04-25')
 
-may_week1 = get_week(data, '2020-05-02')
-may_week2 = get_week(data, '2020-05-09')
-may_week3 = get_week(data, '2020-05-16')
-may_week4 = get_week(data, '2020-05-23')
-may_week5 = get_week(data, '2020-05-30')
+apr_cases1 = get_week(data, '2020-04-04')[0]
+apr_cases1['month'] = 'April'
+apr_cases2 = get_week(data, '2020-04-11')[0]
+apr_cases2['month'] = 'April'
+apr_cases3 = get_week(data, '2020-04-18')[0]
+apr_cases3['month'] = 'April'
+apr_cases4 = get_week(data, '2020-04-25')[0]
+apr_cases4['month'] = 'April'
 
-june_week1 = get_week(data, '2020-06-06')
-june_week2 = get_week(data, '2020-06-13')
-june_week3 = get_week(data, '2020-06-20')
-june_week4 = get_week(data, '2020-06-27')
+may_cases1 = get_week(data, '2020-05-02')[0]
+may_cases1['month'] = 'May'
+may_cases2 = get_week(data, '2020-05-09')[0]
+may_cases2['month'] = 'May'
+may_cases3 = get_week(data, '2020-05-16')[0]
+may_cases3['month'] = 'May'
+may_cases4 = get_week(data, '2020-05-23')[0]
+may_cases4['month'] = 'May'
+may_cases5 = get_week(data, '2020-05-30')[0]
+may_cases5['month'] = 'May'
 
-july_week1 = get_week(data, '2020-07-03')
+june_cases1 = get_week(data, '2020-06-06')[0]
+june_cases1['month'] = 'June'
+june_cases2 = get_week(data, '2020-06-13')[0]
+june_cases2['month'] = 'June'
+june_cases3 = get_week(data, '2020-06-20')[0]
+june_cases3['month'] = 'June'
+june_cases4 = get_week(data, '2020-06-27')[0]
+june_cases4['month'] = 'June'
 
-weeks_list = [jan_week, feb_week1, feb_week2, feb_week3, feb_week4, mar_week1,
-              mar_week2, mar_week3, mar_week4, apr_week1, apr_week2, 
-              apr_week3, apr_week4, may_week1, may_week2, may_week3, 
-              may_week4, may_week5, june_week1, june_week2, june_week3, 
-              june_week4, july_week1]
+july_cases1 = get_week(data, '2020-07-03')[0]
+july_cases1['month'] = 'July'
 
-months_list = ['January', 'February', 'February', 'February', 'February', 
-               'March', 'March', 'March', 'March', 'April', 'April', 'April', 
-               'April', 'May', 'May', 'May', 'May', 'May', 'June', 'June', 
-               'June', 'June', 'July']
+weeks_list = [jan_cases, feb_cases1, feb_cases2, feb_cases3, feb_cases4, mar_cases1,
+              mar_cases2, mar_cases3, mar_cases4, apr_cases1, apr_cases2, 
+              apr_cases3, apr_cases4, may_cases1, may_cases2, may_cases3, 
+              may_cases4, may_cases5, june_cases1, june_cases2, june_cases3, 
+              june_cases4, july_cases1]
 
-def get_plot(week, month):
+
+def cases_plot(week):
     week = week[::-1]
     state_list = list(week.index.values)
     fig, ax = plt.subplots(figsize=(15, 8))
 
-    ax.barh(state_list, week['cases'])
+    ax.barh(state_list, week['cases'], color=[state_color[x] for x in state_list])
     for i, (value, name) in enumerate(zip(week['cases'], state_list)):
-        ax.text(value, i,     name,size=14, weight=600, ha='left', va='bottom')
-        ax.text(value, i -.25, f'{value:,.0f}',  size=14, ha='right',  va='center')
+        ax.text(value, i,     name,size=18, weight=600, ha='left', va='bottom')
+        ax.text(value, i -.25, f'{value:,.0f}',  size=18, ha='right',  va='center')
     
     ax.text(0, 1.12, 'Covid-19 total reported cases by state',
         transform=ax.transAxes, size=24, weight=600, ha='left')
@@ -98,11 +134,89 @@ def get_plot(week, month):
     ax.set_yticks([])
     ax.margins(0, 0.01)
     ax.grid(which='major', axis='x', linestyle='-')
-    ax.text(1, 0.4, month, transform=ax.transAxes, size=46, ha='right')
+    ax.text(1, 0.4, week['month'][0], transform=ax.transAxes, size=46, ha='right')
+    ax.set_axisbelow(True)
+
+feb_deaths4 = get_week(data, '2020-02-29')[1]
+feb_deaths4['month'] = 'February'
+
+mar_deaths1 = get_week(data, '2020-03-07')[1]
+mar_deaths1['month'] = 'March'
+mar_deaths2 = get_week(data, '2020-03-14')[1]
+mar_deaths2['month'] = 'March'
+mar_deaths3 = get_week(data, '2020-03-21')[1]
+mar_deaths3['month'] = 'March'
+mar_deaths4 = get_week(data, '2020-03-28')[1]
+mar_deaths4['month'] = 'March'
+
+
+apr_deaths1 = get_week(data, '2020-04-04')[1]
+apr_deaths1['month'] = 'April'
+apr_deaths2 = get_week(data, '2020-04-11')[1]
+apr_deaths2['month'] = 'April'
+apr_deaths3 = get_week(data, '2020-04-18')[1]
+apr_deaths3['month'] = 'April'
+apr_deaths4 = get_week(data, '2020-04-25')[1]
+apr_deaths4['month'] = 'April'
+
+may_deaths1 = get_week(data, '2020-05-02')[1]
+may_deaths1['month'] = 'May'
+may_deaths2 = get_week(data, '2020-05-09')[1]
+may_deaths2['month'] = 'May'
+may_deaths3 = get_week(data, '2020-05-16')[1]
+may_deaths3['month'] = 'May'
+may_deaths4 = get_week(data, '2020-05-23')[1]
+may_deaths4['month'] = 'May'
+may_deaths5 = get_week(data, '2020-05-30')[1]
+may_deaths5['month'] = 'May'
+
+june_deaths1 = get_week(data, '2020-06-06')[1]
+june_deaths1['month'] = 'June'
+june_deaths2 = get_week(data, '2020-06-13')[1]
+june_deaths2['month'] = 'June'
+june_deaths3 = get_week(data, '2020-06-20')[1]
+june_deaths3['month'] = 'June'
+june_deaths4 = get_week(data, '2020-06-27')[1]
+june_deaths4['month'] = 'June'
+
+july_deaths1 = get_week(data, '2020-07-03')[1]
+july_deaths1['month'] = 'July'
+
+weeks_deaths = [feb_deaths4, mar_deaths1,
+              mar_deaths2, mar_deaths3, mar_deaths4, apr_deaths1, apr_deaths2, 
+              apr_deaths3, apr_deaths4, may_deaths1, may_deaths2, may_deaths3, 
+              may_deaths4, may_deaths5, june_deaths1, june_deaths2, june_deaths3, 
+              june_deaths4, july_deaths1]
+
+
+def deaths_plot(week):
+    week = week[::-1]
+    state_list = list(week.index.values)
+    fig, ax = plt.subplots(figsize=(15, 8))
+
+    ax.barh(state_list, week['deaths'], color=[state_color[x] for x in state_list])
+    for i, (value, name) in enumerate(zip(week['deaths'], state_list)):
+        ax.text(value, i,     name,size=18, weight=600, ha='left', va='bottom')
+        ax.text(value, i -.25, f'{value:,.0f}',  size=18, ha='right',  va='center')
+    
+    ax.text(0, 1.12, 'Covid-19 total reported deaths by state',
+        transform=ax.transAxes, size=24, weight=600, ha='left')
+    ax.xaxis.set_major_formatter(ticker.StrMethodFormatter('{x:,.0f}'))
+    ax.xaxis.set_ticks_position('top')
+    ax.tick_params(axis='x', colors='#777777', labelsize=12)
+    ax.set_yticks([])
+    ax.margins(0, 0.01)
+    ax.grid(which='major', axis='x', linestyle='-')
+    ax.text(1, 0.4, week['month'][0], transform=ax.transAxes, size=46, ha='right')
     ax.set_axisbelow(True)
 
 for i in range(0, len(weeks_list)):
-    get_plot(weeks_list[i], months_list[i])
+    cases_plot(weeks_list[i])
+    
+for i in range(0, len(weeks_deaths)):
+    deaths_plot(weeks_deaths[i])
+    
+
 
 '''
 from matplotlib.animation import FuncAnimation 
